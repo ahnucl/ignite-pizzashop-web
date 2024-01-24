6f806f1ec5a78f8b24303b88d313445ab9e1db63
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-01-24 09:52:11 -0300

    Adidiona skeletons durante carregamento

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,10 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // Cookies do frontend são automaticamente enviados
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    return config
+  })
+}
